5070fd3eb3f19c37c56a2a7f7e721407759ba445
Jeff Deville <jeffdeville@gmail.com>
2018-10-18 19:02:13 -0400

    WIP

--- scripts/gassist-installer-pi3.sh
+++ scripts/gassist-installer-pi3.sh
@@ -25,14 +25,14 @@ then
     exec sudo -u $RUN_AS $0
 fi
 clear
-echo ""
-read -r -p "Enter the your full credential file name including the path and .json extension: " credname
-echo ""
-read -r -p "Enter the your Google Cloud Console Project-Id: " projid
-echo ""
-read -r -p "Enter the modelid that was generated in the actions console: " modelid
-echo ""
-echo "Your Model-Id used for the project is: $modelid" >> /home/${USER}/modelid.txt
+# echo ""
+# read -r -p "Enter the your full credential file name including the path and .json extension: " credname
+# echo ""
+# read -r -p "Enter the your Google Cloud Console Project-Id: " projid
+# echo ""
+# read -r -p "Enter the modelid that was generated in the actions console: " modelid
+# echo ""
+# echo "Your Model-Id used for the project is: $modelid" >> /home/${USER}/modelid.txt
 cd /home/${USER}/
 sudo apt-get update -y
 sudo apt-get install python-pip -y
@@ -52,8 +52,8 @@ pip install google-assistant-library==1.0.0
 pip install google-assistant-grpc==0.2.0
 pip install google-assistant-sdk==0.5.0
 pip install google-assistant-sdk[samples]==0.5.0
-google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype \
-          --scope https://www.googleapis.com/auth/gcm \
-          --save --headless --client-secrets $credname
-echo "Testing the installed google assistant. Make a note of the generated Device-Id"
-googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
+# google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype \
+#           --scope https://www.googleapis.com/auth/gcm \
+#           --save --headless --client-secrets $credname
+# echo "Testing the installed google assistant. Make a note of the generated Device-Id"
+# googlesamples-assistant-hotword --project_id $projid --device_model_id $modelid
