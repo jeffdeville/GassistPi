2db66c25924cf79c82c258ab60663097617077f4
Jeff Deville <jeffdeville@gmail.com>
2018-10-18 21:56:56 -0400

    WIP

--- src/hogwarts.py
+++ src/hogwarts.py
@@ -8,10 +8,10 @@ ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
 
 def award(house, points):
     print('hogwarts.award')
-    subprocess.Popen(["aplay", "{}/sample-audio-files/snowboy.wav".format(ROOT_PATH)],
-                     stdin=subprocess.PIPE,
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE)
+    # subprocess.Popen(["aplay", "{}/sample-audio-files/snowboy.wav".format(ROOT_PATH)],
+    #                  stdin=subprocess.PIPE,
+    #                  stdout=subprocess.PIPE,
+    #                  stderr=subprocess.PIPE)
     print('sound played')
     request = {'queryResult': {'parameters': {'number': points, 'House': house}}}
     r = requests.post(
