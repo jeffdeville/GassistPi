7e181d9eb28ff6fb8f7d63686b51b22798f6e02f
Jeff Deville <jeffdeville@gmail.com>
2018-10-27 06:02:02 -0400

    enable severus

--- src/hogwarts.py
+++ src/hogwarts.py
@@ -8,14 +8,18 @@ ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
 
 def award(house, points):
     print('hogwarts.award')
-    # subprocess.Popen(["aplay", "{}/sample-audio-files/snowboy.wav".format(ROOT_PATH)],
-    #                  stdin=subprocess.PIPE,
-    #                  stdout=subprocess.PIPE,
-    #                  stderr=subprocess.PIPE)
-    print('sound played')
-    # request = {'queryResult': {'parameters': {'number': points, 'House': house}}}
-    # r = requests.post(
-    #     'https://gryffindor.duckdns.org/award/', auth=('hogwarts', 'r%ZSFc5&01b6C9M'), json=request)
-    # resp = r.json()
-    # print(resp)
+    if points in [25, 50, 75, 100]:
+        # request = {'queryResult': {'parameters': {'number': points, 'House': house}}}
+        # r = requests.post(
+        #     'https://gryffindor.duckdns.org/award/', auth=('hogwarts', 'r%ZSFc5&01b6C9M'), json=request)
+        # resp = r.json()
+        # print(resp)
+        print "Points awarded"
+        return None
+    else:
+        subprocess.Popen(["aplay", "{}/sample-audio-files/HP-SS-careful.wav".format(ROOT_PATH)],
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+        print('sound played')
     return None
